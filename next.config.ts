0572325b56ba5f9ca54dd52a7b4f9da3fa9690b3
AlexLeiba <Alexleiba13@gmail.com>
2026-03-12 19:04:17 +0200

    feat(security) headers

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,15 @@
 import type { NextConfig } from "next";
 
+const ContentSecurityPolicy = `
+              default-src 'self';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.clerk.com;
+              style-src 'self' 'unsafe-inline' https:;
+              img-src 'self' data: https://*.clerk.com  https://picsum.photos https://images.unsplash.com https://res.cloudinary.com ;
+              font-src 'self' https: data:;
+              connect-src 'self' https://*.clerk.com;
+              frame-ancestors 'none';
+            `.replace(/\n/g, "");
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
@@ -9,7 +19,7 @@ const nextConfig: NextConfig = {
       "picsum.photos",
       "images.unsplash.com",
       "res.cloudinary.com",
-    ], // ← whitelist Clerk images
+    ], //for next/image optimization
   },
   async headers() {
     return [
@@ -18,16 +28,24 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value: `
-              default-src 'self';
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' https:;
-              style-src 'self' 'unsafe-inline' https:;
-              img-src 'self' data: https:;
-              font-src 'self' https: data:;
-              connect-src 'self' https:;
-              frame-ancestors 'none';
-            `.replace(/\n/g, ""),
-            //iframes none / api calls: https only / fonsts: my site or https only / images: my site or https / js: my site + https / css: my site + inline style
+            value: ContentSecurityPolicy,
+            //iframes none / api calls: https only / fonts: my site or https only / images: my site or https / js: my site + https / css: my site + inline style
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
           },
         ],
       },

fb5e966bbc74941c87ff67831cd6e501c692665c
AlexLeiba <Alexleiba13@gmail.com>
2026-04-14 11:55:49 +0300

    feat:(dashboard-table view) optimize select board view tab

--- components/Protected/Pages/Board/SubHeader/BoardTabSections.tsx
+++ components/Protected/Pages/Board/SubHeader/BoardTabSections.tsx
@@ -1,9 +1,11 @@
 import { IconButton } from "@/components/ui/iconButton";
+import { useDebounce } from "@/hooks/useDebounce";
 import { BOARD_HEADER_TABS } from "@/lib/consts/protected/board";
 
 import { BoardTabSectionType } from "@/lib/types";
 import { cn } from "@/lib/utils";
 import { useStore } from "@/store/useStore";
+import { useState } from "react";
 import { useShallow } from "zustand/shallow";
 
 export function BoardTabSections() {
@@ -13,10 +15,15 @@ export function BoardTabSections() {
       boardTabSections: state.boardTabSections,
     })),
   );
+  const [localSelectedTab, setLocalSelectedTab] =
+    useState<BoardTabSectionType>(boardTabSections);
+  const debounceSelectTab = useDebounce(setBoardTabSections, 100);
+
+  // to much time takes to update this state, need to optimize it, maybe move it to local state and then update the global state after 1 second of debounce, because right now when i click on any tab section it takes around 1 second to update the state and show the selected tab section, and it makes bad user experience, because user think that the click is not working and click multiple times on the same tab section, and then after 1 second all the clicks are registered and it shows the last clicked tab section, but if i move the state to local state and then update the global state after 1 second of debounce, it will show the selected tab section immediately and then after 1 second it will update the global state, and it will make better user experience
 
   function handleSelectTabSection(tab: BoardTabSectionType) {
-    // TODO, change zustand views based on selected view
-    setBoardTabSections(tab);
+    setLocalSelectedTab(tab);
+    debounceSelectTab(tab);
   }
   return (
     <div className=" items-center gap-2 md:flex hidden">
@@ -27,7 +34,7 @@ export function BoardTabSections() {
             title={`Open ${tab.label} view`}
             onClick={() => handleSelectTabSection(tab.value)}
             className={cn(
-              boardTabSections === tab.value &&
+              localSelectedTab === tab.value &&
                 "outline-1 outline-gray-300 rounded-md",
               "flex flex-col w-full p-1.5",
             )}

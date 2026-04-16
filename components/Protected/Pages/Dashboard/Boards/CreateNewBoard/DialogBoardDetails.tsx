9297ceb7ecd33513610679e3b261bf1c610764f4
AlexLeiba <Alexleiba13@gmail.com>
2026-04-16 15:21:53 +0300

    fix:(dashboard) create board no organization selected error

--- components/Protected/Pages/Dashboard/Boards/CreateNewBoard/DialogBoardDetails.tsx
+++ components/Protected/Pages/Dashboard/Boards/CreateNewBoard/DialogBoardDetails.tsx
@@ -13,15 +13,16 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { InputTitleSchema, InputTitleSchemaType } from "@/lib/schemas";
 import { UnsplashImagesType } from "@/lib/types";
 import toast from "react-hot-toast";
-import { usePathname, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { createNewBoardAction } from "@/app/actions/dashboard";
 import { useStore } from "@/store/useStore";
 import { type Board } from "@/lib/generated/prisma/client";
 import { QUERY_KEYS } from "@/lib/query-mutation-keys/keys";
 import { UNSPLASH_DEFAULT_IMAGES } from "@/lib/consts/protected/files";
+import { useAuth } from "@clerk/nextjs";
 
 type Props = {
-  type?: "dashboard" | "board";
+  type?: "dashboard" | "header";
 };
 export function DialogBoardDetails({ type = "dashboard" }: Props) {
   const navigate = useRouter();
@@ -30,12 +31,8 @@ export function DialogBoardDetails({ type = "dashboard" }: Props) {
     (state) => state.setNewBoardDialogOpen,
   );
   const queryClient = useQueryClient();
-  const pathname = usePathname();
 
-  const organizationId =
-    type === "dashboard"
-      ? pathname.split("/").at(-1)
-      : pathname.split("/").at(-3);
+  const { orgId } = useAuth();
 
   const { data, isFetching } = useQuery({
     queryFn: getUnsplashImagesAction,
@@ -54,8 +51,8 @@ export function DialogBoardDetails({ type = "dashboard" }: Props) {
 
         toast.success("Board created successfully");
 
-        if (type === "board") {
-          navigate.push(`/dashboard/${organizationId}/board/${data?.id}`);
+        if (type === "header") {
+          navigate.push(`/dashboard/${orgId}/board/${data?.id}`);
         }
       },
       onError: ({ message }) => toast.error(message || "Error creating board"),
@@ -83,7 +80,7 @@ export function DialogBoardDetails({ type = "dashboard" }: Props) {
       setError("title", { message: "Please select an image" });
       return toast.error("Please select an image");
     }
-    if (!organizationId) {
+    if (!orgId) {
       return toast.error("Please select an organization");
     }
 
@@ -95,7 +92,7 @@ export function DialogBoardDetails({ type = "dashboard" }: Props) {
       cardImageUrl:
         selectedImage?.urls.small || selectedImage?.urls.regular || "",
       bgImageUrl: selectedImage?.urls.full || selectedImage?.urls.regular || "",
-      orgId: organizationId,
+      orgId: orgId,
     };
     mutateCreateNewBoard(newBoardData);
   }
@@ -103,7 +100,7 @@ export function DialogBoardDetails({ type = "dashboard" }: Props) {
   return (
     <div
       className="flex flex-col md:gap-12 gap:4"
-      data-test="dialog-board-details"
+      data-test={`dialog-board-details-${type}-container`}
     >
       <div className="grid md:grid-cols-[repeat(auto-fit,minmax(140px,1fr))] grid-cols-[repeat(auto-fit,minmax(100px,1fr))] gap-2">
         {isFetching ? (
